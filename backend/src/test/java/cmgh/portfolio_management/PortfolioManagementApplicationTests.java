759cc2375e1a27a31611496a90be53669829d05b
Huy Cao <huy.cao@solenix.ch>
2026-01-28 23:08:47 +0100

    Initial setup

---
+++ backend/src/test/java/cmgh/portfolio_management/PortfolioManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cmgh.portfolio_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
